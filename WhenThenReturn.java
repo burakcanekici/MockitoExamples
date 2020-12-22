f4e621e9791915e78f5f5e35bf90bc6c5acc4bf5
Burakcan Ekici <bcekici11@gmail.com>
2020-12-22 15:30:41 +0300

    Update

--- WhenThenReturn.java
+++ WhenThenReturn.java
@@ -1,7 +1,8 @@
 public class Triangle{
   private int area;
   
-  //getter & setter
+  public int getArea(){ return area; }
+  public void setArea(int area){ this.area = area; }
 }
 
 public class MathService {
@@ -25,6 +26,11 @@ public class MathServiceTest {
   public void shouldSuccessWhenCalculateMath(){
     when(triangle.getArea()).thenReturn(35);
     service.calculateMath(triangle);
+    
+    // unit test getter & setter
+    //when(triangle.getArea()).thenReturn(55);
+    //doCallRealMethod().when(triangle).setArea(55);
+    
     assertEquals(triangle.getArea(), 35);
   }
 }
